d2b3899fa033d77a5dd56d2d217265bada7f6150
wakdj <j.armstrongwalsh1@uni.brighton.ac.uk>
2024-03-27 18:10:12 +0000

    added text message type chat

--- all/chatbot/static/script.js
+++ all/chatbot/static/script.js
@@ -1,30 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const textInput = document.getElementById('textInput');
-    const predictButton = document.getElementById('predictButton');
-    const outputArea = document.getElementById('outputArea');
 
-    predictButton.addEventListener('click', function () {
-        const message = textInput.value.trim();
-        if (message !== '') {
-            fetch('/predict', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ message: message })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                outputArea.textContent = data.answer;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-        }
-    });
-});
+    
+
+function predictMessage() {
+    const message = textInput.value.trim();
+    if (message !== '') {
+        fetch('/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: message })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Fail');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const newLiForBot = document.createElement("li");
+            const newLiForUser = document.createElement("li");
+            const responseText = document.createTextNode(data.answer);
+            const userText = document.createTextNode(message);
+            newLiForBot.appendChild(responseText);
+            newLiForUser.appendChild(userText);
+            userMessage.appendChild(newLiForUser);
+            outputArea.appendChild(newLiForBot)
+            
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const textInput = document.querySelector('#textInput');
+    const predictButton = document.querySelector('#predictButton');
+    const outputArea = document.querySelector('#outputArea > ul');
+    const userMessage = document.querySelector("#userMessage > ul");
+    predictButton.addEventListener('click', predictMessage);
+})
+
+
+
+console.log("Borders are not staying. Just a guideline")
