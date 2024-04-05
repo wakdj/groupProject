adff13a8b428d556390ce8b47bb573c209e6b40f
wakdj <j.armstrongwalsh1@uni.brighton.ac.uk>
2024-04-05 21:29:21 +0100

    fixed compounding misalignment

--- all/chatbot/static/script.js
+++ all/chatbot/static/script.js
@@ -27,11 +27,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 const newLiForBot = document.createElement("li");
                 const newLiForUser = document.createElement("li");
                 const responseText = document.createTextNode(data.answer);
+                
                 const userText = document.createTextNode(message);
                 newLiForBot.appendChild(responseText);
                 newLiForUser.appendChild(userText);
                 userMessage.appendChild(newLiForUser);
                 outputArea.appendChild(newLiForBot);
+                adjustHeightDifference(userMessage,outputArea);
+                // console.log(outputArea.getClientRects()[0].height)
+                // console.log(userMessage.getClientRects()[0].height)
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -41,3 +45,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
     predictButton.addEventListener('click', predictMessage);
 });
+
+function adjustHeightDifference(userMessage,outputArea) {
+    const chatResponseHeight = outputArea.getClientRects()[0].height;
+    const userInputHeight = userMessage.getClientRects()[0].height;
+    if(chatResponseHeight !== userInputHeight){
+        const difference = chatResponseHeight - userInputHeight
+        const lastUserListItem = userMessage.lastChild;
+        // const lastUserListItemStyles = window.getComputedStyle(lastUserListItem);
+        // const lastUserListItemPaddingBottom = parseFloat(lastUserListItemStyles.paddingBottom);
+        
+        const lastBotListItem = outputArea.lastChild;
+
+        const lastUserListItemHeight = lastUserListItem.clientHeight;
+        const lastBotListItemHeight = lastBotListItem.clientHeight;
+        // const lastBotListItemStyles = window.getComputedStyle(lastBotListItem);
+        // const lastBotListItemStylesPaddingBottom = parseFloat(lastBotListItemStyles.paddingBottom);
+        // console.log(lastUserListItemPaddingBottom)
+        // console.log(lastBotListItemStylesPaddingBottom)
+        console.log(lastBotListItemHeight)  
+        lastUserListItem.style.height = (lastBotListItemHeight) + "px"
+        lastUserListItem.style.padding = 0
+        lastUserListItem.style.color = "red" 
+        //userMessage.lastChild.style.paddingTop = -difference
+    }
+
+ }
